46984957d2c663eed9d5541db6c6ae0961994df6
zlogzr <zylogzr@163.com>
2022-05-18 17:44:17 +0800

    首次修改

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+      this is home-page
+    </div>
+  )
+}
+export default HomePage
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+const LoginPage = () => {
+  return (
+    <div className='login-page'>
+      this is login-page
+    </div>
+  )
+}
+export default LoginPage
