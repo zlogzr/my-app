28b9ddf225faa49fdc3b7f86200a79eebd9d39bd
zhangyang <2224143549@qq.com>
2022-05-22 23:37:40 +0800

    懒加载demo

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,15 @@
+import { Button } from 'bellejs'
 import './style.css'
 
-const LoginPage = () => {
+
+const LoginPage = (props: any) => {
+  const handleLogin = () => {
+    props.history.push('/manage')
+  }
   return (
     <div className='login-page'>
-      this is login-page
+      <h2 className='content'>this is login-page</h2>
+      <Button className='loginBtn' type='primary' onClick={handleLogin}>登录</Button>
     </div>
   )
 }
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,3 +1,45 @@
-import Routers from './routers'
+import { Component, lazy, Suspense } from 'react'
+import { HashRouter as Router, Redirect, Switch } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+import { ConfigProvider, Spin } from 'bellejs'
+import ZhCN from 'bellejs/es/locale/zh_CN'
 
-export default Routers
+// import ManagePage from '../pages/manage'
+// import LoginPage from '../pages/login'
+
+const ManagePage = lazy(() => import('../pages/manage'))
+const LoginPage = lazy(() => import('../pages/login'))
+
+const allRouters = [
+  {
+    path: '/manage',
+    exact: true,
+    component: ManagePage,
+    title: 'manage',
+  },
+  {
+    path: '/login',
+    exact: true,
+    component: LoginPage,
+    title: 'login',
+  },
+]
+
+class AppRouter extends Component {
+  render() {
+    return (
+      <ConfigProvider locale={ZhCN}>
+        <Suspense fallback={<Spin style={{ margin: '10px auto' }} />}>
+          <Router>
+            <Switch>
+              <Redirect exact from='/' to='/login' />
+              {renderRoutes(allRouters.map((item) => ({ ...item, key: item.path })))}
+            </Switch>
+          </Router>
+        </Suspense>
+      </ConfigProvider >
+    )
+  }
+}
+
+export default AppRouter
